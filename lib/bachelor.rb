b643e4630a19c1a73427c7bd8962b1bef3086986
Ishmet <ishmetjahan@yahoo.com>
2019-06-23 00:09:51 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,65 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
+  data[season].each do |name|
+        name.each do |data, type|
+        if type == "Winner"
+          return ele[:name].split(" ").first 
+        end
+      end
+      return "No Winner"
+    end
+  end
+
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, arr|
+    arr.each do |ele|
+      ele.each do |data, type|
+      if type == occupation
+        return ele["name"]
+      end
+    end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+count=0 
+data.each do |season,arr|
+  arr.each do |ele|
+    ele.each do |data, type|
+    if type == hometown
+      count +=1 
+    end
+  end
+end
+end
+return count 
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, arr|
+    arr.each do |ele|
+      ele.each do |data,type|
+        if type == hometown 
+        return ele["occupation"]
+      end
+    end
+  end
+end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age=0 
+  count=0 
+  data[season].each do |ele|
+    ele.each do |data,type|
+      if data== "age"
+        count +=1 
+        age += type.to_f
+        #str to float
+      end
+    end
+  end
+  ans= (age/count).round
+  return ans 
 end
